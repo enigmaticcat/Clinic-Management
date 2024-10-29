646942413baa7c1e5b2d2f8212e5f36328e76cff
enigmaticcat <tranquangminh_hsgs19@hus.edu.vn>
2024-10-30 00:31:35 +0700

    hfdus

--- front-end/src/pages/Doctors.jsx
+++ front-end/src/pages/Doctors.jsx
@@ -3,18 +3,16 @@ import { useParams, useNavigate } from 'react-router-dom'
 import { AppContext } from '../context/AppContext'
 
 const Doctors = () => {
-
     const { speciality } = useParams()
-    const [filterDoc, selfFilterDoc] = useState([])
+    const [filterDoc, setFilterDoc] = useState([])
     const navigate = useNavigate()
-
     const { doctors } = useContext(AppContext)
 
     const applyFilter = () => {
         if (speciality) {
-            selfFilterDoc(doctors.filter(doc => doc.speciality === speciality))
+            setFilterDoc(doctors.filter(doc => doc.speciality === speciality))
         } else {
-            selfFilterDoc(doctors)
+            setFilterDoc(doctors)
         }
     }
 
@@ -24,20 +22,47 @@ const Doctors = () => {
 
     return (
         <div>
-            <p > Browse through the doctors specialists.</p>
-            <div>
-                <div>
-                    <p>General physican</p>
-                    <p>Gynecologist</p>
-                    <p>Dermatologist</p>
-                    <p>Pediatricians</p>
-                    <p>Neurologist</p>
-                    <p>Gastroenterologist</p>
+            <p className='text-gray-600'> Browse through the doctors specialists.</p>
+            <div className='flex flex-col sm:flex-row items-start gap-5 mt-5'>
+                <div className='flex flex-col gap-4 text-sm text-gray-600'>
+                    <p
+                        onClick={() => navigate('/doctors/General physician')}
+                        className={`w-[94vw] sm:w-auto pl-3 py-1.5 pr-16 border border-gray-300 rounded transition-all cursor-pointer ${speciality === "General physician" ? "bg-indigo-100 text-black" : ""}`}>
+                        General physician
+                    </p>
+                    <p
+                        onClick={() => navigate('/doctors/Gynecologist')}
+                        className={`w-[94vw] sm:w-auto pl-3 py-1.5 pr-16 border border-gray-300 rounded transition-all cursor-pointer ${speciality === "Gynecologist" ? "bg-indigo-100 text-black" : ""}`}>
+                        Gynecologist
+                    </p>
+                    <p
+                        onClick={() => navigate('/doctors/Dermatologist')}
+                        className={`w-[94vw] sm:w-auto pl-3 py-1.5 pr-16 border border-gray-300 rounded transition-all cursor-pointer ${speciality === "Dermatologist" ? "bg-indigo-100 text-black" : ""}`}>
+                        Dermatologist
+                    </p>
+                    <p
+                        onClick={() => navigate('/doctors/Pediatricians')}
+                        className={`w-[94vw] sm:w-auto pl-3 py-1.5 pr-16 border border-gray-300 rounded transition-all cursor-pointer ${speciality === "Pediatricians" ? "bg-indigo-100 text-black" : ""}`}>
+                        Pediatricians
+                    </p>
+                    <p
+                        onClick={() => navigate('/doctors/Neurologist')}
+                        className={`w-[94vw] sm:w-auto pl-3 py-1.5 pr-16 border border-gray-300 rounded transition-all cursor-pointer ${speciality === "Neurologist" ? "bg-indigo-100 text-black" : ""}`}>
+                        Neurologist
+                    </p>
+                    <p
+                        onClick={() => navigate('/doctors/Gastroenterologist')}
+                        className={`w-[94vw] sm:w-auto pl-3 py-1.5 pr-16 border border-gray-300 rounded transition-all cursor-pointer ${speciality === "Gastroenterologist" ? "bg-indigo-100 text-black" : ""}`}>
+                        Gastroenterologist
+                    </p>
                 </div>
                 <div className='w-full grid grid-cols-auto gap-4 gap-y-6'>
                     {
                         filterDoc.map((item, index) => (
-                            <div onClick={() => navigate(`/appointment/${item._id}`)} className='border border-blue-200 rounded-xl overflow-hidden cursor-pointer hover:translate-y-[-10px] transition-all duration-500' key={index}>
+                            <div
+                                onClick={() => navigate(`/appointment/${item._id}`)}
+                                className='border border-blue-200 rounded-xl overflow-hidden cursor-pointer hover:translate-y-[-10px] transition-all duration-500'
+                                key={index}>
                                 <img className='bg-blue-50' src={item.image} alt='' />
                                 <div className='p-4'>
                                     <div className='flex items-center gap-2 text-sm text-center text-green-500'>
