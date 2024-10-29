c4b652956118b1a17d54e8adcfdec5fcec9b9a1f
enigmaticcat <tranquangminh_hsgs19@hus.edu.vn>
2024-10-29 22:23:55 +0700

    2nd change

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -9,6 +9,7 @@ import About from './pages/About';
 import Contact from './pages/Contact';
 import MyProfile from './pages/MyProfile';
 import MyAppointments from './pages/MyAppointments';
+import Footer from "./components/Footer";
 const App = () => {
   return (
     <div className="mx-4 sm:mx-[10%]">
@@ -24,6 +25,7 @@ const App = () => {
         <Route path='./my-appointments' element={<MyAppointments />} />
         <Route path='./appointments/:docId' element={<Appointment />} />
       </Routes>
+      <Footer />
     </div>
   )
 }
