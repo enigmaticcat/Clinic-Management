cf093523d96ce3202fab9346e13b0e03c2050e2f
enigmaticcat <tranquangminh_hsgs19@hus.edu.vn>
2024-11-19 17:20:31 +0700

    init

--- front-end/src/pages/Appointment.jsx
+++ front-end/src/pages/Appointment.jsx
@@ -1,8 +1,32 @@
-import React from 'react'
-
+import React, { useState, useContext, useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+import {AppContext} from '../context/AppContext'
+ 
 const Appointment = () => {
+
+    const { docId } = useParams()
+    const { doctors } = useContext(AppContext)
+    const [docInfo,setDocInfo] = useState(null)
+    
+
+    const fetchDocInfo = async () => {
+        const docInfo = doctors.find(doc => doc._id === docId)
+        setDocInfo(docInfo)
+        console.log(docInfo);
+    }
+    useEffect(()=>{
+        fetchDocInfo()
+    },[doctors,docId])
     return (
-        <div></div>
+        <div>
+            {/*------------Doctor Details-------------*/}
+            <div>
+                <div>
+                    <img src={docInfo.image} alt="" />
+                </div>
+            </div>
+
+        </div>
     )
 }
 
