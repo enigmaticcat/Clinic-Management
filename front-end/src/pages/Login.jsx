4a002b92b46af8c9a2ae429522803731a3903d1c
enigmaticcat <tranquangminh_hsgs19@hus.edu.vn>
2024-11-27 00:12:52 +0700

    27/11

--- front-end/src/pages/Login.jsx
+++ front-end/src/pages/Login.jsx
@@ -25,15 +25,15 @@ const Login = () => {
                 }
                 <div className='w-full'>
                     <p>Full Name</p>
-                    <input className='border border-zinc-300 rounded w-full p-2 mt-1' type = "text" onChange={(e) => setName(e.target.name)} value = {name} />
+                    <input className='border border-zinc-300 rounded w-full p-2 mt-1' type = "text" onChange={(e) => setName(e.target.value)} value = {name} />
                 </div>
                 <div className='w-full'>
                     <p>Email</p>
-                    <input className='border border-zinc-300 rounded w-full p-2 mt-1' type = "email" onChange={(e) => setEmail(e.target.name)} value = {email} />
+                    <input className='border border-zinc-300 rounded w-full p-2 mt-1' type = "email" onChange={(e) => setEmail(e.target.value)} value = {email} />
                 </div>
                 <div className='w-full'>
                     <p>Password</p>
-                    <input className='border border-zinc-300 rounded w-full p-2 mt-1' type = "password" onChange={(e) => setPassword(e.target.name)} value = {password} />
+                    <input className='border border-zinc-300 rounded w-full p-2 mt-1' type = "password" onChange={(e) => setPassword(e.target.value)} value = {password} />
                 </div>
                 <button className='bg-primary text-white w-full py-2 rounded-md text-base'>{state === 'Sign Up' ? "Create Account" : "Login"}</button>
                 {
